88c588d90496552314e5eb135b56f62a3d600185
ynorouzz <ynr.info@gmail.com>
2019-04-23 15:30:06 +0200

    Load

--- productsale.py
+++ productsale.py
@@ -1,21 +1,11 @@
 
-# import matplotlib
-# matplotlib.use('TkAgg')  # TkAgg
-# import matplotlib.pyplot as plt
 import pandas as pd
-# import json
-# import numpy as np
 
 dataPath = './Data/'
 
 # Load json data to dataframe
 data = pd.read_json(dataPath+'20190207_transactions.json', orient='columns', lines=True)
 
-# Select product ids
-products = pd.DataFrame(data["products"].tolist())
-
-# Print frequency of products
-print(' How many times products are sold on Feb 7:  \n', products.count())
 
 
 
